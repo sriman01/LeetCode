8d489ec5cace62f1cc1088445b5897f27e2928e2
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-05-18 15:37:05 +0530

    Added solution - LeetHub

--- 7. Find the number of islands - GFG/7.-find-the-number-of-islands.cpp
+++ 7. Find the number of islands - GFG/7.-find-the-number-of-islands.cpp
@@ -19,7 +19,7 @@ class Solution {
          if(grid[i][j] != '1') 
           return; 
         
-        grid[i][j]=2;
+        grid[i][j]=0;
         
        dfs(i-1,j,grid);
        dfs(i+1,j,grid);
