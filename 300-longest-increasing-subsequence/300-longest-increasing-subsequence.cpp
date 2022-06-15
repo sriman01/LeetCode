3e4ee4294589b341cbf59d17499ff7332a47f8e0
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-06-15 15:04:39 +0530

    Time: 1853 ms (5.02%), Space: 292.2 MB (6.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+    
+    int LIS(int n, int m,int size, vector<int>& nums, vector<vector<int>>& dp) {
+        if(n == size)
+            return 0;
+        
+        if(dp[n][m + 1] != -1) return dp[n][m + 1];
+        
+       int length = LIS(n+1, m, size, nums, dp);
+        
+        if(m == -1 || nums[ m ] < nums[ n ]) 
+            length = max(length, 1 + LIS(n+1, n, size, nums, dp));
+            
+        
+        return dp[ n ][m + 1] = length;
+        
+             
+        
+    }
     int lengthOfLIS(vector<int>& nums) {
         
-        int n = nums.size();
+        int size = nums.size();
         
-        int i,j;
-        int mixi = 0;
+        vector<vector< int >> dp(size, vector<int>(size+1, -1));
         
-        vector<int>dp(nums.size(), 1);
-    
-        for(int i = 1;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1)
-                {
-                    dp[i] = dp[j] + 1;
-                }
-            }
-        }
-        
-        for(auto x : dp)
-        {
-            if(x > mixi)
-            {
-                mixi = x;
-            }
-                
-        }
-        
-        return mixi;
-        
+          return LIS(0, -1, size, nums, dp);
     }
 };
