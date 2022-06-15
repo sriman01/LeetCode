03563f2c10f8d2cafbb05f4b651bf5ce61871059
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-06-15 23:39:50 +0530

    Time: 2 ms (39.73%), Space: 6 MB (45.52%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int kthGrammar(int n, int k) {
         
-        if(n==1 )
-            return 0;
+         if(n == 1 && k == 1)
+             return 0;
         
-        int x = kthGrammar( n - 1, ceil( k / 2.0 ));
+        int mid = pow(2 , n-1)/2;
         
-        if( k & 1 )
-            return x;
+        if(k <= mid)
+            return kthGrammar(n - 1, k);
         else
-            return ! x;
+            return ! kthGrammar(n - 1, k - mid);
         
     }
 };
