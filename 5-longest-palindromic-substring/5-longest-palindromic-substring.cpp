8378b2e8e4a90658d3a1d3a34f51681cc9441555
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-06-17 00:25:27 +0530

    Time: 1037 ms (9.74%), Space: 387 MB (7.17%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,49 +1,34 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-     int n=s.size(); 
+         int n= s.size(), length = 0, start = 0;
         
-       
-        vector<vector<bool>> dp(n, vector<bool> (n,false));
+        vector<vector<int>> dp(n, vector<int> (n, 0));
         
-       
-        for(int i=0; i<n; i++) dp[i][i]=true;
-        
-        int start=0, lenMax=1;
-        
-       
-        for(int i=0; i<n-1; i++) 
-        {
-            if(s[i]==s[i+1])
-            {
-                dp[i][i+1] = true;
-                start = i;
-                lenMax = 2;
-            }
+        for(int i = 0; i < n ; i ++) {
+            dp[i][i] = 1;
+            
+            if(i < n - 1 && s[i] == s[i + 1])
+                dp[i][i+1] = 1;
         }
         
-        
-        for(int k=3; k<=n; k++)  
-        {
-            for(int i=0; i<n-k+1; i++) 
-            {
-                int j=i+k-1;
-                
-               
-                if(dp[i+1][j-1]==true && s[i]==s[j])
-                {
-                    dp[i][j] = true;
+        for(int i = n- 3; i >= 0; i --) {
+            for(int j = i + 2; j < n; j ++) {
+                if( s[i] == s[j] && dp[i + 1][j - 1])
                     
-                    if(k>lenMax)
-                    {
-                        lenMax = k;
-                        start=i;
-                    }
-                }
+                    dp[i][j] = 1;
             }
         }
         
-        return s.substr(start, lenMax); 
+        for(int i = 0; i < n; i ++) {
+            for(int j = i; j < n; j++) {
+                if(dp[i][j] && length < j - i + 1)
+                    length = j - i + 1, start = i;
+            }
+        }
+        
+
+        return s.substr(start, length);
         
     }
 };
