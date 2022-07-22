59d4fe58d620407c7af71c2fd25eb1edc5b8c452
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-07-22 23:23:42 +0530

    Time: 15 ms (32.68%), Space: 10.9 MB (37.57%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -3,22 +3,19 @@ public:
     vector<vector<int>> res;
     
     void Sum2(int idx, vector<int> &cand, vector<int> &temp, int target, int n) {
-        if(idx == n) {
-            if(target == 0){
+      
+         if(target == 0){
                 res.push_back(temp);
-            }
             return;
         }
     
-    if(target >= cand[idx]){
-        temp.push_back(cand[idx]);
-        Sum2(idx + 1, cand, temp, target - cand[idx], n);
-        temp.pop_back(); 
-    }
-        idx ++;
-        
-        while(idx < n && cand[idx - 1] == cand[idx]) idx++;
-        Sum2(idx, cand, temp, target, n);
+    for(int i = idx; i < cand.size(); i++){
+        if(i > idx && cand[i] == cand[i - 1]) continue;
+        if(target < cand[i]) break;
+        temp.push_back(cand[i]);
+        Sum2(i+1, cand, temp, target - cand[i], n);
+        temp.pop_back();
+      }
 
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
