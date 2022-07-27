018a5430e7809fb7524d1b2593532be0469c626d
Sriman Kumar <82316398+sriman01@users.noreply.github.com>
2022-07-28 00:11:44 +0530

    Time: 11 ms (54.57%), Space: 7.2 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,46 +1,65 @@
 class Solution {
-    vector<vector<string>> ans;
+    bool isSafe(int row, int col, vector<string> &container, int n){
+        int drow = row;
+        int dcol = col;
+        
+        while(row >= 0 && col >= 0){
+            if(container[row][col] == 'Q')
+                return false;
+            row --;
+            col --;
+            }
+        
+        row = drow;
+        col = dcol;
+        
+        while( col >= 0){
+            if(container[row][col] == 'Q')
+                return false;
+            col --;
+        }
+        
+        col = dcol;
+        
+        while( row < n && col >= 0){
+            if(container[row][col] == 'Q')
+                return false;
+            
+            col --;
+            row ++;
+            
+        }
+        
+        return true;
+    }
 public:
-	bool issafe(int r,int c,vector<string>&v,int n){
-		
-		for(int i=0;i<=r;i++){
-			if(v[i][c]=='Q')
-                return false;
-		}
-	
-		int row=r,col=c;
-		while(row>=0 && col>=0){
-			if(v[row][col]=='Q') 
-                return false;
-			row--,col--;
-		}
-	
-		row=r,col=c;
-		while(row>=0 && col<n){
-			if(v[row][col]=='Q')
-                return false;
-			row--,col++;
-		}
-		return true;
-	}
-	void helper(int r,vector<string> &v,int n){
-		 if(r==n){ 
-			 ans.push_back(v);
-			 return ;
-		 }
-		for(int i=0;i<n;i++){  
-			if(issafe(r,i,v,n)){
-				v[r][i]='Q';
-				helper(r+1,v,n);
-				v[r][i]='.';
-			}
-		}
-	}
-	vector<vector<string>> solveNQueens(int n) {
-		vector<string> v;
-		string s(n,'.');
-		for(int i=0;i<n;i++) v.push_back(s);
-		helper(0,v,n);
-		return ans;
-	}
+    void Solve(int col, vector<string> &container, vector<vector<string>> &res, int n) {
+        if(col == n) {
+            res.push_back(container);
+            return;
+        }
+        for(int row = 0; row < n; row++) {
+            if(isSafe(row, col, container, n)){
+                container[row][col] = 'Q';
+                Solve(col + 1, container, res, n);
+                container[row][col] = '.';
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> container;
+        
+        string s(n, '.');
+        
+        for(int i = 0;i < n; i ++){
+            container.push_back(s);
+        }
+        
+        Solve(0, container, res, n);
+        
+        return res;
+        
+    }
 };
